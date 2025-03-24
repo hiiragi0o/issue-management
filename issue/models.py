b150a651c324941435cb9902593aef887fdcaddc
Margherita88 <lilac.spring2403@gmail.com>
2025-03-24 23:39:30 +0900

    second comitt

--- issue/models.py
+++ issue/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Issues(models.Model):
     title = models.CharField(max_length=50)
+    contents = models.TextField(blank=True,null=True)
     record = models.TextField(blank=True,null=True)
+    date_of_update = models.DateField(auto_now=True)
+    deadline = models.DateField(blank=True,null=True)
     type = models.IntegerField(choices=[(0,"不具合"),(1,"タスク"),(2,"要望"),(3,"その他")])
-    amount = models.IntegerField()
-    date = models.DateField()
+    person = models.IntegerField(choices=[(0,"担当A"),(1,"担当B"),(2,"担当C"),(3,"担当D")])
+    progress = models.IntegerField(choices=[(0,"完了"),(1,"進行中"),(2,"未対応"),(3,"保留"),(4,"対応不要")])
+    budget = models.IntegerField(blank=True,null=True)
+
