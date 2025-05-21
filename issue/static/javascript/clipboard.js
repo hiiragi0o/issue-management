acc9435466383e3160937a98f8ed4693e821fd75
Margherita88 <lilac.spring2403@gmail.com>
2025-05-21 17:58:31 +0900

    “jsファイルをまとめる”

---
+++ issue/static/javascript/clipboard.js
@@ -0,0 +1,21 @@
+// クリップボードコピーのJS
+
+// HTML要素取得
+const txt = document.getElementById('txt');
+const btn = document.getElementById('btn');
+
+// コピー処理（btnがクリックされたらtxtをコピーする）
+btn.addEventListener('click', () => {
+    if (!navigator.clipboard) {
+        alert("残念ですが、このブラウザは対応していません...");
+        return;
+    }
+
+    navigator.clipboard.writeText(txt.textContent).then(
+        () => {
+            alert('リンクをクリップボードにコピーできました！\nメール本文などに貼り付けて利用できます。');
+        },
+        () => {
+            alert('クリップボードにコピーできませんでした。');
+        });
+});
