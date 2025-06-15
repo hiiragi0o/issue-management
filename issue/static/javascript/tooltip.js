d001128c99ec2b45d1354d3a0cef79165877c5a7
Margherita88 <lilac.spring2403@gmail.com>
2025-06-15 21:44:05 +0900

    ツールチップ位置を修正

--- issue/static/javascript/tooltip.js
+++ issue/static/javascript/tooltip.js
@@ -1,3 +1,6 @@
 // BootstrapツールチップのJS
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
+    container: 'body', // ツールチップを body に配置して位置ズレを防ぐ
+    placement: 'auto', // 位置を自動調整
+}));
